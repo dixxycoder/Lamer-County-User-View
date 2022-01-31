aaef907d7d74d94119e1b57efe19e40f729f7961
Grant Spraker <grant.spraker@gmail.com>
2022-01-31 11:52:21 -0700

    getting there

---
+++ Lamer County/Map_Controller.swift
@@ -0,0 +1,23 @@
+//
+//  Map_Controller.swift
+//  Lamer County
+//
+//  Created by Grant Spraker on 1/31/22.
+//
+
+import UIKit
+import MapKit
+
+class Map_Controller: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
