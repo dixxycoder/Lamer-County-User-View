c20e2321b3ca319e94b6dc0be928a45300d463a4
Grant Spraker <grant.spraker@gmail.com>
2022-03-21 11:50:14 -0600

    working on the MVP of the user view. for lamar county

---
+++ lamar county  take 2/lamar county  take 2/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  lamar county  take 2
+//
+//  Created by Grant Spraker on 3/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource
+{
+    // the tVFE stands for tableViewForEvents.
+    @IBOutlet var tVFE: UITableView!
+    
+    var event : [Event] = []
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        tVFE.dataSource = self
+        
+        let event1 = Event(name:"boys & girls")
+        let event2 = Event(name:"bick")
+        event = [event1,event2]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return event.count
+        
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "events", for: indexPath)
+        let currentItem = event[indexPath.row]
+        cell.textLabel?.text = currentItem.name
+        return cell
+    }
+    
+
+}
+
---
+++ lamar county  take 2/lamar county  take 2/eventS.swift
@@ -0,0 +1,18 @@
+//
+//  eventS.swift
+//  lamar county  take 2
+//
+//  Created by Grant Spraker on 3/21/22.
+//
+
+import Foundation
+
+class Event
+{
+    var name: String
+    
+    init(name:String)
+    {
+        self.name = name
+    }
+}
